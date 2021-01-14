cae2c3e0295ade609cd9523a07958fc1b91ca045
yosri3 <yosri.jerbi@hotmail.com>
2021-01-14 15:41:36 +0100

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -12,15 +12,15 @@ function navBar (){
 function makeCard (imgUlr, title, description, modalID, contribution){
 	document.write('\
 		<div class="col-md-4">\
-							<div class="card ">\
-								<img src="'+ imgUlr +'" class="card-img-top" alt="...">\
-								<div class="card-body">\
-									<h5 class="card-title">'+title+'</h5>\
-									<p class="card-text">'+description+'</p>\
-									<p class="card-text">'+contribution+'</p>\
-									<a href="#" class="stretched-link" data-toggle="modal" data-target="'+modalID+'"></a>\
-								</div>\
-							</div>\
-						</div>\
+			<div class="card ">\
+				<img src="'+ imgUlr +'" class="card-img-top" alt="...">\
+				<div class="card-body">\
+					<h5 class="card-title">'+title+'</h5>\
+					<p class="card-text">'+description+'</p>\
+					<p class="card-text">'+contribution+'</p>\
+					<a href="#" class="stretched-link" data-toggle="modal" data-target="'+modalID+'"></a>\
+				</div>\
+			</div>\
+		</div>\
 	');
 }
